eab143195329756a536d6e7e23000572e4929122
Dodla Prasanna Kumar Reddy <prasannakumarreddy003@gmail.com>
2025-09-01 18:28:58 +0530

    Time: 855 ms (13.17%), Space: 0B (100%) - LeetHub

---
+++ 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT b.employee_id,b.name,count(a.reports_to)as reports_count,ROUND(AVG(a.age),0)AS average_age
+FROM Employees a
+JOIN Employees b
+ON a.reports_to=b.employee_id
+GROUP BY b.employee_id,b.name
+ORDER BY b.employee_id;
