8db6811719a0ab124a51c6f074124c18330a49f9
Dodla Prasanna Kumar Reddy <prasannakumarreddy003@gmail.com>
2025-09-28 13:57:23 +0530

    Time: 0 ms (100%), Space: 42.6 MB (15.72%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,6 +1,6 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int y=m+n;
+        /*int y=m+n;
         int i=0,j=0,k=0;
         int [] temp=new int [y];
         for(int x=0;x<y;x++){
@@ -27,7 +27,24 @@ class Solution {
             nums1[i]=nums2[k];
             k++;
             i++;
-        }
+        } */
+ //from back pointers
+  int x1=m+n-1;
+  int x2=m-1;
+  int x3=n-1;
+  while(x1>=0 && x2>=0 && x3>=0){
+    if(nums1[x2]>=nums2[x3]){
+        nums1[x1]=nums1[x2--];
+    }else{
+        nums1[x1]=nums2[x3--];
+    }
+    x1--;
+  }
+  while(x1>=0&& x3>=0){
+    nums1[x1--]=nums2[x3--];
+  }
+        
+        
 
 
 
