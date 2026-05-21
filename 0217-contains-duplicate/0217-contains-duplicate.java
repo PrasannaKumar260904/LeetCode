64a51ed733182e8c90c6230f278adee307828c36
Dodla Prasanna Kumar Reddy <prasannakumarreddy003@gmail.com>
2026-05-21 16:18:09 +0530

    Time: 14 ms (88.75%), Space: 93.6 MB (32.31%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -20,11 +20,11 @@ class Solution {
         // }
         // return false;
         HashSet<Integer> set=new HashSet<>();
-        for(int i=0;i<nums.length;i++){
-            if(set.contains(nums[i])){
+        for(int num:nums){
+            if(set.contains(num)){
                 return true;
             }
-            set.add(nums[i]);
+            set.add(num);
         }
         return false;
 
