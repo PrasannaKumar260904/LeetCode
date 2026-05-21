8549b3a233779ac5dcf6f21cf7a7621231abad86
Dodla Prasanna Kumar Reddy <prasannakumarreddy003@gmail.com>
2026-05-21 18:47:50 +0530

    Time: 25 ms (39.64%), Space: 76.4 MB (68.26%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,18 +1,30 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        if(k==0) return false;
-        HashMap<Integer,Integer>map=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            if(map.containsKey(nums[i])){
+        // if(k==0) return false;
+        // HashMap<Integer,Integer>map=new HashMap<>();
+        // for(int i=0;i<nums.length;i++){
+        //     if(map.containsKey(nums[i])){
                  
-                if(i-map.get(nums[i])<=k){
-                    return true;
-                }
+        //         if(i-map.get(nums[i])<=k){
+        //             return true;
+        //         }
+        //     }
+        //     map.put(nums[i],i);
+
+        // }
+        // return false;
+        if(nums == null||nums.length<=1||k<=0)return false;
+        HashSet<Integer> set=new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(set.contains(nums[i])){
+                return true;
             }
-            map.put(nums[i],i);
+            set.add(nums[i]);
+            if(set.size()>k){
+                set.remove(nums[i-k]);
+            }
 
         }
         return false;
-        
     }
 }
