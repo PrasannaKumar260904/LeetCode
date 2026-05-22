d662746e7c0a3be075405b3d70a3dd44eb2b0252
Dodla Prasanna Kumar Reddy <prasannakumarreddy003@gmail.com>
2026-05-22 07:18:26 +0530

    Time: 20 ms (14.27%), Space: 49.9 MB (28.95%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,17 +1,37 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         //hashmap based solution key:string value :list<of grouped anagrams>
+        // if(strs==null||strs.length==0)return new ArrayList<>();
+        // HashMap<String,List<String>> map=new HashMap<>();
+        // for(String s:strs){
+        //     char [] ch=s.toCharArray();
+        //     Arrays.sort(ch);
+        //     String sortedKey=String.valueOf(ch);
+        //     map.putIfAbsent(sortedKey,new ArrayList<>());
+        //     map.get(sortedKey).add(s);
+
+        // }
+        // return new ArrayList<>(map.values());
         if(strs==null||strs.length==0)return new ArrayList<>();
         HashMap<String,List<String>> map=new HashMap<>();
+        int [] count =new int[26];
         for(String s:strs){
-            char [] ch=s.toCharArray();
-            Arrays.sort(ch);
-            String sortedKey=String.valueOf(ch);
-            map.putIfAbsent(sortedKey,new ArrayList<>());
-            map.get(sortedKey).add(s);
+            Arrays.fill(count,0);
+            for(char ch:s.toCharArray()){
+                count[ch-'a']++;
+            }
+            StringBuilder sb=new StringBuilder("");
+            for(int i=0;i<26;i++){
+                sb.append('#');
+                sb.append(count[i]);
+            }
+            String key=sb.toString();
+            map.putIfAbsent(key,new ArrayList<>());
+            map.get(key).add(s);
+            
 
         }
-        return new ArrayList<>(map.values());
+        return new ArrayList <> (map.values());
         
     }
 }
