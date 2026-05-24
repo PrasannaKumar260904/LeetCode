f4a9628a8652bf999f707f4dc857c1359ba7d139
Dodla Prasanna Kumar Reddy <prasannakumarreddy003@gmail.com>
2026-05-24 11:40:13 +0530

    Time: 0 ms (100%), Space: 43.1 MB (50.44%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -14,20 +14,34 @@
  * }
  */
 class Solution {
-    public void preOrder(TreeNode node,List<Integer> arr){
-        if(node==null){
-            return;
-        }
-        arr.add(node.val);
-        preOrder(node.left,arr);
-        preOrder(node.right,arr);
+    // public void preOrder(TreeNode node,List<Integer> arr){
+    //     if(node==null){
+    //         return;
+    //     }
+    //     arr.add(node.val);
+    //     preOrder(node.left,arr);
+    //     preOrder(node.right,arr);
         
-    }
+    // }
     
     public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> arr=new ArrayList<>();
-        preOrder(root,arr);
-        return arr;
+        // List<Integer> arr=new ArrayList<>();
+        // preOrder(root,arr);
+        // return arr;
+        List<Integer> result=new ArrayList<>();
+        if(root==null)return result;
+        Stack<TreeNode> st=new Stack<>();
+        st.push(root);
+        while(!st.isEmpty()){
+            TreeNode curr=st.pop();
+            int data=curr.val;
+            result.add(data);
+            if(curr.right!=null)st.push(curr.right);
+            if(curr.left!=null)st.push(curr.left);
+
+        }
+        return result;
+
 
         
     }
