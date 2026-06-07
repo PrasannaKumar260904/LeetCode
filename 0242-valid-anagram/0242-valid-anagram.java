9a5e8367bfd674824b80d54297c350336e96e852
Dodla Prasanna Kumar Reddy <prasannakumarreddy003@gmail.com>
2026-06-07 08:24:43 +0530

    Time: 2 ms (99.64%), Space: 45 MB (49.88%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,17 +1,39 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length())return false;
-        int [] count=new int[26];
-        for(int i=0;i<s.length();i++){
-            count[s.charAt(i)-'a']++;
-            count[t.charAt(i)-'a']--;
-        
+        char[] freq=new char[26];
+        char[] ch1=s.toCharArray();
+        char[] ch2=t.toCharArray();
+        for(char ch:ch1){
+            freq[ch-'a']++;
+            
         }
-        for(int x:count){
-            if(x!=0)return false;
+        for(char ch:ch2){
+            freq[ch-'a']--;
+        }
+        for(char count:freq){
+            if(count!=0){
+                return false;
+            }
         }
         return true;
+        
+        //char[] ch1=s.toCharArray();
+        // char[] ch2=t.toCharArray();
+        // Arrays.sort(ch1);
+        // Arrays.sort(ch2);
+        // if(s.length()!=t.length())return false;
+        // for(char i=0;i<ch1.length;i++){
+        //     if(ch1[i]!=ch2[i]){
+        //         return false;
+        //     }
+        // }
+        // return true;
 
+
+
+
+
+        
         
     }
 }
