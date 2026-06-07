a8b74266a270ca6c01a6576f7350951d332792c5
Dodla Prasanna Kumar Reddy <prasannakumarreddy003@gmail.com>
2026-06-07 09:20:05 +0530

    Time: 15 ms (54.89%), Space: 47.6 MB (64.5%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,34 +1,34 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer,Integer> freq=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            freq.put(nums[i],freq.getOrDefault(nums[i],0)+1);
-        }
-        //convert map entries to list
-        List<Map.Entry<Integer,Integer>> list=new ArrayList<>(freq.entrySet());
-        list.sort((a,b)->b.getValue()-a.getValue());
-        int [] ans=new int[k];
-        for(int i=0;i<ans.length;i++){
-            ans[i]=list.get(i).getKey();
-        }
-        return ans;
         // HashMap<Integer,Integer> freq=new HashMap<>();
         // for(int i=0;i<nums.length;i++){
         //     freq.put(nums[i],freq.getOrDefault(nums[i],0)+1);
         // }
-        // PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)->freq.get(a)-freq.get(b));
-        // for(int x:freq.keySet()){
-        //     pq.offer(x);
-        //     if(pq.size()>k){
-        //         pq.poll();
-        //     }
-        // }
+        // //convert map entries to list
+        // List<Map.Entry<Integer,Integer>> list=new ArrayList<>(freq.entrySet());
+        // list.sort((a,b)->b.getValue()-a.getValue());
         // int [] ans=new int[k];
         // for(int i=0;i<ans.length;i++){
-        //     ans[i]=pq.poll();
-
+        //     ans[i]=list.get(i).getKey();
         // }
         // return ans;
+        HashMap<Integer,Integer> freq=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            freq.put(nums[i],freq.getOrDefault(nums[i],0)+1);
+        }
+        PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)->freq.get(a)-freq.get(b));
+        for(int x:freq.keySet()){
+            pq.offer(x);
+            if(pq.size()>k){
+                pq.poll();
+            }
+        }
+        int [] ans=new int[k];
+        for(int i=0;i<ans.length;i++){
+            ans[i]=pq.poll();
+
+        }
+        return ans;
 
 
         
