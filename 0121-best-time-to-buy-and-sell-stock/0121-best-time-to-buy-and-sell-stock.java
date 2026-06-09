5073635c291e57e3021a6d34adaa7868bc8a9a3c
Dodla Prasanna Kumar Reddy <prasannakumarreddy003@gmail.com>
2026-06-09 15:42:31 +0530

    Time: 1 ms (99.93%), Space: 94.3 MB (68.49%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,14 +1,29 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int n=prices.length;
-        int minprice=Integer.MAX_VALUE;
-        int maxprofit=0;
+        // int n=prices.length;
+        // int minprice=Integer.MAX_VALUE;
+        // int maxprofit=0;
 
-        for(int i=0;i<n;i++){
-            minprice=Math.min(minprice,prices[i]);
-            maxprofit=Math.max(maxprofit,prices[i]-minprice);
+        // for(int i=0;i<n;i++){
+        //     minprice=Math.min(minprice,prices[i]);
+        //     maxprofit=Math.max(maxprofit,prices[i]-minprice);
+        // }
+        // return maxprofit;
+        //sliding window approach
+        int maxProfit=0;
+        int left=0;
+        int right=1;
+        while(right<prices.length){
+            if(prices[right]>prices[left]){
+                int profit=prices[right]-prices[left];
+                maxProfit=Math.max(maxProfit,profit);
+            }else{
+                left=right;
+            }
+            right++;
         }
-        return maxprofit;
+        return maxProfit;
+
         
     }
 }
